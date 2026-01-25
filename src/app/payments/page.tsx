e4432a8d73bcd6d39f99613f6d083eb8ac532bed
jasonwj96 <jasonwj96@gmail.com>
2026-01-25 11:30:00 -0500

    Payments form css changes

--- src/app/payments/page.tsx
+++ src/app/payments/page.tsx
@@ -1,29 +1,32 @@
-'use client';
+"use client";
 
-import { loadStripe } from '@stripe/stripe-js';
-import PaymentForm from '@/components/PaymentForm';
-import styles from '@/app/payments/page.module.css';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import Navbar from "@/components/Navbar";
+import { loadStripe } from "@stripe/stripe-js";
+import PaymentForm from "@/components/PaymentForm";
+import styles from "@/app/payments/page.module.css";
+import { useRouter } from "next/navigation";
+import { useState, useEffect, useCallback } from "react";
 
+import { Elements } from "@stripe/react-stripe-js";
 
-import {
-  Elements
-} from '@stripe/react-stripe-js';
-
-const stripePromise = loadStripe(`${process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY}`);
+const stripePromise = loadStripe(
+  `${process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY}`,
+);
 
 export default function PaymentPage() {
   const router = useRouter();
+  const [currentTokens, setCurrentTokens] = useState(0);
 
-  useEffect(() => {
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <div className={styles.paymentsContainer}>
-      <Elements stripe={stripePromise}>
+      <Navbar currentPage="/payments" currentTokens={currentTokens} />
+      <div className={styles.paymentFormContainer}>
+            <Elements stripe={stripePromise}>
         <PaymentForm />
-      </Elements>
+      </Elements>  
+      </div>
     </div>
   );
 }
