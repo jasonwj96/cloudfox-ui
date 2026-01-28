c6c3f4b6e32ddabab30bf66e6c73f0d4d98956be
jasonwj96 <jasonwj96@gmail.com>
2026-01-27 23:12:04 -0500

    payment

--- src/components/PaymentForm.tsx
+++ src/components/PaymentForm.tsx
@@ -1,18 +1,17 @@
 "use client";
 
-import Link from 'next/link';
-import styles from '@/components/PaymentForm.module.css';
-import { useEffect, useState } from 'react';
+import Link from "next/link";
+import styles from "@/components/PaymentForm.module.css";
+import { useEffect, useState } from "react";
 import {
   CardNumberElement,
   CardExpiryElement,
   CardCvcElement,
   useStripe,
-  useElements
-} from '@stripe/react-stripe-js';
+  useElements,
+} from "@stripe/react-stripe-js";
 
 export default function PaymentForm() {
-
   const stripe = useStripe();
   const elements = useElements();
 
@@ -26,25 +25,25 @@ export default function PaymentForm() {
   const PRICE_PER_UNIT = 0.001;
 
   const CARD_NUMBER_ELEMENT_OPTIONS = {
-    placeholder: 'XXXX XXXX XXXX XXXX',
+    placeholder: "XXXX XXXX XXXX XXXX",
     style: {
       base: {
-        fontFamily: 'Quicksand, sans-serif',
-        fontStyle: 'normal',
-        fontSize: '1.2em',
-        color: '#ffffff',
+        fontFamily: "Quicksand, sans-serif",
+        fontStyle: "normal",
+        fontSize: "1.2em",
+        color: "#ffffff",
         fontWeight: 100,
-        backgroundColor: 'transparent',
-        wordSpacing: '5px',
-        letterSpacing: '1px',
-        padding: '5px',
-        border: 'none',
-        '::placeholder': {
-          color: '#969696ff',
+        backgroundColor: "transparent",
+        wordSpacing: "5px",
+        letterSpacing: "1px",
+        padding: "5px",
+        border: "none",
+        "::placeholder": {
+          color: "#969696ff",
         },
       },
       invalid: {
-        color: '#fe0000ff',
+        color: "#fe0000ff",
       },
     },
   };
@@ -53,20 +52,20 @@ export default function PaymentForm() {
     placeholder: "cvc",
     style: {
       base: {
-        textAlign: 'center',
-        fontFamily: 'Quicksand, sans-serif',
-        fontStyle: 'normal',
-        fontSize: '1em',
-        lineHeight: '1.5',
-        fontWeight: 'bold',
-        color: '#303030',
-        letterSpacing: '1px',
-        '::placeholder': {
-          color: '#a0aec0',
+        textAlign: "center",
+        fontFamily: "Quicksand, sans-serif",
+        fontStyle: "normal",
+        fontSize: "1em",
+        lineHeight: "1.5",
+        fontWeight: "bold",
+        color: "#303030",
+        letterSpacing: "1px",
+        "::placeholder": {
+          color: "#a0aec0",
         },
       },
       invalid: {
-        color: '#fe0000ff',
+        color: "#fe0000ff",
       },
     },
   };
@@ -74,20 +73,20 @@ export default function PaymentForm() {
   const CARD_DATE_ELEMENT_OPTIONS = {
     style: {
       base: {
-        textAlign: 'center',
-        fontFamily: 'Quicksand, sans-serif',
-        fontStyle: 'normal',
-        fontSize: '0.9em',
-        lineHeight: '1.5',
-        fontWeight: '100',
-        color: '#ffffff',
-        letterSpacing: '1px',
-        '::placeholder': {
-          color: '#a0aec0',
+        textAlign: "center",
+        fontFamily: "Quicksand, sans-serif",
+        fontStyle: "normal",
+        fontSize: "0.9em",
+        lineHeight: "1.5",
+        fontWeight: "100",
+        color: "#ffffff",
+        letterSpacing: "1px",
+        "::placeholder": {
+          color: "#a0aec0",
         },
       },
       invalid: {
-        color: '#fe0000ff',
+        color: "#fe0000ff",
       },
     },
   };
@@ -101,43 +100,19 @@ export default function PaymentForm() {
   }, []);
 
   const createPaymentIntent = async () => {
-    const user = localStorage.getItem("user");
+    let  url = `${process.env.NEXT_PUBLIC_API_URL}:${process.env.NEXT_PUBLIC_API_PORT}/cloudfox-api/v1/payment/intent`;
+      
+    const res = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      body: JSON.stringify({
+        tokenAmount
+      }),
+    });
 
-    if (user) {
-      if (!tokenAmount || tokenAmount <= 0) return;
-
-      try {
-        let url = `${process.env.NEXT_PUBLIC_API_URL}:${process.env.NEXT_PUBLIC_API_PORT}/api/create-payment-intent`;
-
-        await fetch(url, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            amount: tokenAmount,
-            pricePerUnit: PRICE_PER_UNIT
-          }),
-        }).then(response => response.json())
-          .then(json => {
-            setClientSecret(json.clientSecret);
-
-            url = `${process.env.NEXT_PUBLIC_API_URL}:${process.env.NEXT_PUBLIC_API_PORT}/api/add-tokens`;
-
-            fetch(url, {
-              method: "POST",
-              body: JSON.stringify({
-                user: JSON.parse(user),
-                tokenAmount
-              })
-            });
-
-            alert(`${tokenAmount} tokens added successfully to your account.`);
-
-          });
-      } catch (err) {
-        console.error(err);
-        setError("Failed to initialize payment.");
-      }
-    }
+    const  response = await res.json();
+    console.log(response);
   };
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -146,7 +121,7 @@ export default function PaymentForm() {
     await createPaymentIntent();
 
     if (!stripe || !elements || !clientSecret) {
-      setError('Stripe has not loaded yet.');
+      setError("Stripe has not loaded yet.");
       return;
     }
 
@@ -156,7 +131,7 @@ export default function PaymentForm() {
     const cardNumberElement = elements.getElement(CardNumberElement);
 
     if (!cardNumberElement) {
-      setError('Card details not entered.');
+      setError("Card details not entered.");
       setIsProcessing(false);
       return;
     }
@@ -170,26 +145,27 @@ export default function PaymentForm() {
     setIsProcessing(false);
 
     if (result.error) {
-      setError(result.error.message || 'Payment failed.');
-    } else if (result.paymentIntent && result.paymentIntent.status === 'succeeded') {
-
+      setError(result.error.message || "Payment failed.");
+    } else if (
+      result.paymentIntent &&
+      result.paymentIntent.status === "succeeded"
+    ) {
       const url = `${process.env.NEXT_PUBLIC_API_URL}:${process.env.NEXT_PUBLIC_API_PORT}/api/buy-tokens`;
       await fetch(url, {
         method: "POST",
         body: JSON.stringify({
           username: "",
-          tokenAmount
-        })
+          tokenAmount,
+        }),
       })
-        .then(response => response.json())
-        .then(json => {
-          alert('Payment successful!');
+        .then((response) => response.json())
+        .then((json) => {
+          alert("Payment successful!");
         });
     } else {
-      setError('Payment failed or was cancelled.');
+      setError("Payment failed or was cancelled.");
     }
   };
-
 
   return (
     <div className={styles.paymentsFormContainer}>
@@ -212,7 +188,10 @@ export default function PaymentForm() {
                 <div className={styles.cardIssuerLogo}>
                   <div className={styles.creditCardIssuer}>Credit Card</div>
                 </div>
-                <img src="credit-card-chip.png" className={styles.creditCardChip} />
+                <img
+                  src="credit-card-chip.png"
+                  className={styles.creditCardChip}
+                />
               </div>
               <div className={styles.cardDetails}>
                 <div className={styles.cardRow1}>
@@ -245,9 +224,7 @@ export default function PaymentForm() {
                 <div className={styles.holoCard} />
                 <div className={styles.cardRow3}>
                   <div className={styles.holocardNumber}>
-                    <CardCvcElement
-                      options={CARD_CVC_ELEMENT_OPTIONS}
-                    />
+                    <CardCvcElement options={CARD_CVC_ELEMENT_OPTIONS} />
                   </div>
                 </div>
               </div>
@@ -255,7 +232,9 @@ export default function PaymentForm() {
           </div>
           <div className={styles.paymentFormCheckout}>
             <div className={styles.paymentFormCheckoutAmount}>
-              <label className={styles.loginFormLabel} htmlFor="login-user">Token amount</label>
+              <label className={styles.loginFormLabel} htmlFor="login-user">
+                Token amount
+              </label>
               <input
                 name="login-user"
                 className={styles.input}
@@ -264,7 +243,7 @@ export default function PaymentForm() {
                 maxLength={30}
                 required
                 value={tokenAmount}
-                onChange={e => {
+                onChange={(e) => {
                   const val = parseInt(e.target.value);
                   if (!isNaN(val)) {
                     setTokenAmount(val);
@@ -273,28 +252,30 @@ export default function PaymentForm() {
                   }
                 }}
               />
-              <p className={`${styles.loginUserError} ${styles.inputErrorMsg}`} style={{ display: 'none' }}></p>
+              <p
+                className={`${styles.loginUserError} ${styles.inputErrorMsg}`}
+                style={{ display: "none" }}
+              ></p>
             </div>
           </div>
-
         </div>
 
         <div className={styles.paymentFormButtons}>
           <Link href="/dashboard" className={styles.returnButton}>
             Back to dashboard
           </Link>
-          <button type="submit"
+          <button
+            type="submit"
             disabled={!stripe || isProcessing}
-            className={styles.buyButton}>
-            {
-              new Intl.NumberFormat('en-US', {
-                style: 'currency',
-                currency: 'USD',
-              }).format(tokenAmount * PRICE_PER_UNIT)
-            }
+            className={styles.buyButton}
+          >
+            {new Intl.NumberFormat("en-US", {
+              style: "currency",
+              currency: "USD",
+            }).format(tokenAmount * PRICE_PER_UNIT)}
           </button>
         </div>
       </form>
     </div>
-  )
+  );
 }
